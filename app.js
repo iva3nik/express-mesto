f029c37056c6dcc467eb627ac8b38ad43e7466c0
Ivan Nikitin <iva3nik@yandex.ru>
2021-06-18 00:01:28 +0300

    feat: models(card and user) have been created

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port and some changes ${PORT}`);
+});
